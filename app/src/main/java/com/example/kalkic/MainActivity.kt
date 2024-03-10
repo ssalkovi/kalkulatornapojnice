8923b17be60501eee6e319ab0b2352b449c91192
Stjepan <stjepan.salkovic@gmail.com>
2024-03-10 17:52:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kalkic/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.kalkic
+
+import android.icu.text.NumberFormat
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kalkic.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
+
+
